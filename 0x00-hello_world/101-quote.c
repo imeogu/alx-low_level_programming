e9f602caff980397add864244b980120eabc9ac1
imeogu <stephenimeogu@gmail.com>
2022-03-10 20:48:53 -0800

    my push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
 }
