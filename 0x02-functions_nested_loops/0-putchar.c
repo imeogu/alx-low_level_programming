7b4c444c9116b5192a63a21d42c04fc924e28414
imeogu <stephenimeogu@gmail.com>
2022-03-15 16:25:25 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-char c[10] = "_putchar";
-int i = 0;
-while (i < 9)
+     char c[10] = "_putchar";
+     int i = 0;
+     while (i < 9)
 {
-_putchar(c[i]);
-i++;
+     _putchar(c[i]);
+     i++;
 }
-_putchar('\n');
-return (0);
+     _putchar('\n');
+      return (0);
 }
