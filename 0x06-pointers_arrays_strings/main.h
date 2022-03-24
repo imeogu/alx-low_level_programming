dd048ba408ccc0a068895940a0f86dc4b6c8da56
imeogu <stephenimeogu@gmail.com>
2022-03-24 11:44:58 -0700

    My commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,54 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/**
-* _putchar - writes the character c to stdout
-*/
-int _putchar(char c);
-
-/**
-* *_strcat - concatenates two strings
-*/
 char *_strcat(char *dest, char *src);
-
-/**
-* *_strncat - concatenates two strings
-*/
 char *_strncat(char *dest, char *src, int n);
-
-/**
-* *_strncpy - copies a string
-*/
 char *_strncpy(char *dest, char *src, int n);
-
-/**
-* _strcmp - compares two strings
-*/
 int _strcmp(char *s1, char *s2);
-
-/**
-* reverse_array - reverses the content of an array of integers
-*/
 void reverse_array(int *a, int n);
-
-/**
-* *string_toupper - changes all lowercase characters of a string to uppercase
-*/
 char *string_toupper(char *);
-
-/**
-* *capt_string - capitalizes all words of a string
-*/
 char *cap_string(char *);
-
-/**
-* *leet - encodes a string into 1337
-*/
 char *leet(char *);
+char *rot13(char *n);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-/**
-* *rot13 - encodes a string using rot13
-*/
-char *rot13(char *);
-
-#endif /* HOLBERTON_H */
+#endif
