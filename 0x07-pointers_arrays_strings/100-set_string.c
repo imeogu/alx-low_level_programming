c29b2e96a872decfcad5f223c9d0bb0e090f3f4a
imeogu <stephenimeogu@gmail.com>
2022-03-28 15:41:04 -0700

    my work

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- *@to: string
- *Return: void
+ * set_string - Sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
